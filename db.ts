53b9abd2d79b994559ce2615563a4e184be20134
Andrei1001 <andreidizschmitt@gmail.com>
2026-09-22 16:46:29 -0300

    Update db.ts

--- db.ts
+++ db.ts
@@ -1,14 +1,27 @@
-import { Database } from "bun:sqlite";
+import { Database } from "bun:sqlite"
 
-const db = new Database("database.sqlite");
+const db = new Database("database.sqlite")
+
 const query = db.query(`
     CREATE TABLE IF NOT EXISTS users (
-        id              INTEGER PRIMARY KEY AUTOINCREMENT,
-        username        TEXT NOT NULL UNIQUE,
-        email           TEXT NOT NULL UNIQUE,
-        password_hash   TEXT NOT NULL
-    );
-`);
-query.run();
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL UNIQUE,
+        password_hash TEXT NOT NULL
+    )
+`)
+
+query.run()
+
+const query2 = db.query(`
+    CREATE TABLE IF NOT EXISTS mensagem (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        texto TEXT NOT NULL,
+        remetente TEXT NOT NULL,
+        data TEXT NOT NULL
+    )
+`)
+
+query2.run()
 
 export { db }
